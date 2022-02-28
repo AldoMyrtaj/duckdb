3aa4978a2ceab8df25e4b20c388bcd7629de73ed
Aldo Myrtaj <aldo.myrtaj@gmail.com>
2022-02-28 14:59:57 +0100

    Removed Exessive Byte Collecting Array

--- extension/parquet/include/parquet_bss_decoder.hpp
+++ extension/parquet/include/parquet_bss_decoder.hpp
@@ -10,12 +10,10 @@ public:
 
 	template <typename T>
 	void GetBatch(char *values_target_ptr, uint32_t batch_size, idx_t result_offset) {
-		constexpr size_t num_streams = sizeof(T);
 		for(int i = 0; i < batch_size; ++i) {
 			uint8_t gathered_byte_data[num_streams];
 			for(size_t b=0; b < num_streams; ++b) {
 				uint64_t byte_index = b * num_values_in_buffer + result_offset + i;
-				gathered_byte_data[b] = buffer_.getValue<uint8_t>(byte_index);
 				values_target_ptr[i*num_streams+b] = buffer_.getValue<uint8_t>(byte_index);
 			}
 		}
